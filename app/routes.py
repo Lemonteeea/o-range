2823f5ee6aa9e00ae3a395e2aa63727a8f3e0607
o-range <982813440@qq.com>
2019-12-17 15:44:34 +0800

    change jay.jpg and music.html

--- app/routes.py
+++ app/routes.py
@@ -20,12 +20,12 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username = form.username.data).first()
+        user = User.query.filter_by(username = form.username.data).first_or_404()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember = form.remember_me.data)
-        return redirect(url_for('index'))
+        return redirect(url_for('user', username = form.username.data))
     return render_template('login.html', title = 'Sign In', form = form)
 
 @app.route('/logout')
