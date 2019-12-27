9ef449d5dcf7377ceeb092e67428c0e17a008d26
o-range <982813440@qq.com>
2019-12-27 18:28:33 +0800

    add .gitignore

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = 'Just-see-if-you-can-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
